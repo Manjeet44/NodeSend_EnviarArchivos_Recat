07a6854e5b5475e7db39aa2f38b2de617e892ec5
Manjeet44 <andrew_vab@hotmail.com>
2022-05-11 21:03:36 +0200

    v1

---
+++ nodesend-cliente/context/auth/authReducer.js
@@ -0,0 +1,14 @@
+import { USUARIO_AUTENTICADO } from '../../types';
+
+
+export default (state, action) => {
+    switch(action.type) {
+        case USUARIO_AUTENTICADO:
+            return {
+                ...state,
+                usuario: action.payload
+            }
+        default: 
+            return state;
+    }
+}
---
+++ nodesend-cliente/context/auth/authState.js
@@ -0,0 +1,40 @@
+import React, {useReducer} from 'react';
+import authContext from "./authContext";
+import authReducer from './authReducer';
+import { USUARIO_AUTENTICADO } from '../../types';
+
+const AuthState = ({children}) => {
+    //Definir un state inicial
+    const initialState = {
+        token: '',
+        autenticado: null,
+        usuario: null,
+        mensaje: null
+    }
+    //Definir el reducer
+    const [ state, dispatch] = useReducer(authReducer, initialState);
+
+    //Usuario autenticado
+    const usuarioAutenticado = nombre => {
+        dispatch({
+            type: USUARIO_AUTENTICADO,
+            payload: nombre
+        })
+    }
+
+    return (
+        <authContext.Provider
+            value={{
+                token: state.token,
+                autenticado: state.autenticado,
+                usuario: state.usuario,
+                mensaje: state.mensaje,
+                usuarioAutenticado
+            }}
+        >
+            {children}
+        </authContext.Provider>
+    )
+}
+
+export default AuthState;
